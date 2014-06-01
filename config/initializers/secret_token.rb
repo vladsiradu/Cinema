9f6a21e37aabe5ee810254315bb8d8855ab91243
vlad <vladsiradu@gmail.com>
2014-06-01 13:29:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cinema::Application.config.secret_key_base = 'e6077284a82061704bddb69a9fc707383bdd4f6a386417fa9de9f12a8277b600b667a694c745611aed7ce41967079e298cc6bb6a675dec9eaf671c3b4575e65c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cinema::Application.config.session_store :cookie_store, key: '_Cinema_session'
